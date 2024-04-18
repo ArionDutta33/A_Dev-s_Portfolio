fd655604951119b08df6aab4962c83718e5cfd5d
ArionDutta33 <ariondutta333@outlook.com>
2024-04-18 21:55:00 +0530

    Added the animations

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-const countDown = () => {
-    let a = 0;
-    setInterval(() => {
-        a = a + Math.floor(Math.random() * 20);
-        if (a <= 100) {
-            document.querySelector(".loader h1").textContent = a + "%";
-        } else {
-            a = 100;
-            document.querySelector(".loader h1").textContent = a + "%"
-        }
+// const countDown = () => {
+//     let a = 0;
+//     setInterval(() => {
+//         a = a + Math.floor(Math.random() * 20);
+//         if (a <= 100) {
+//             document.querySelector(".loader h1").textContent = a + "%";
+//         } else {
+//             a = 100;
+//             document.querySelector(".loader h1").textContent = a + "%"
+//         }
 
 
-    }, 100)
+//     }, 100)
 
-}
+// }
 
 
 
-const t1 = gsap.timeline();
-t1.to(".loader h1", {
-    delay: .5,
-    duration: 1,
-    onStart: countDown()
-})
-t1.to(".loader", {
-    top: "-100vh"
-})
+// const t1 = gsap.timeline();
+// t1.to(".loader h1", {
+//     delay: .5,
+//     duration: 1,
+//     onStart: countDown()
+// })
+// t1.to(".loader", {
+//     top: "-100vh"
+// })
 t1.to(".page1 .sentence", {
     transform: "translateX(-100%)",
+    fontWeight: "100",
     delay: 1,
-    duration: 5
+    duration: 18
 })
 t1.from(".popup", {
 
